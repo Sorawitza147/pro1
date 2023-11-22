1fa314ec81a6358f4612013456b4d29ece5aca5f
Sorawit <2110511106002@live4.utcc.ac.th>
2023-11-22 22:57:59 +0700

    pro1

---
+++ search.php
@@ -0,0 +1,163 @@
+<?php
+require_once 'config.php';
+
+if (isset($_POST['query'])) {
+    $inputText = $_POST['query'];
+
+    // Validate and sanitize input (example, you may need more depending on your requirements)
+    $inputText = filter_var($inputText, FILTER_SANITIZE_STRING);
+
+    $sql = "SELECT country_name FROM place_info WHERE country_name LIKE :country";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute(['country' => '%' . $inputText . '%' ]);
+    $result = $stmt->fetchAll();
+
+    if ($result) {
+        foreach($result as $row) {
+            // Escape output to prevent XSS
+            echo '<a class="list-group-item list-group-item-action border-1">' . htmlspecialchars($row['country_name']) . '</a>';
+        }
+    } else {
+        echo '<p class="list-group-item border-1">No record.</p>';
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search Results</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+
+        .result-container {
+            border: 1px solid #ccc;
+            padding: 15px;
+            margin-bottom: 20px;
+        }
+
+        .btn {
+            display: inline-block;
+            padding: 10px 15px;
+            background-color: #4CAF50;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+
+<body>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search Results</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+
+        .result-container {
+            border: 1px solid #ccc;
+            padding: 15px;
+            margin-bottom: 20px;
+        }
+
+        .btn {
+            display: inline-block;
+            padding: 10px 15px;
+            background-color: #4CAF50;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+
+<body>
+<?php
+// Set the API endpoint
+$apiEndpoint = 'http://localhost/pro1/api.php';
+
+// Get the search term
+$search = isset($_POST['search']) ? $_POST['search'] : '';
+
+// Check if the search term is empty or too short
+if (empty($search) || strlen($search) < 3) {
+    echo 'Please enter a valid search term (at least 3 characters).';
+} else {
+    // Fetch data from the API
+    $apiResponse = callApi($apiEndpoint);
+
+    // Check if there's an error calling the API
+    if ($apiResponse === false) {
+        echo 'Error calling API';
+    } else {
+        // Decode the JSON response
+        $apiData = json_decode($apiResponse, true);
+
+        // Check if JSON decoding was successful
+        if ($apiData === null && json_last_error() !== JSON_ERROR_NONE) {
+            echo 'Error decoding JSON: ' . json_last_error_msg();
+        } else {
+            // Check if there is data from the API
+            if (empty($apiData['data'])) {
+                echo "No data from API";
+            } else {
+                $found = false;
+                // Display results matching the search term
+                foreach ($apiData['data'] as $data) {
+                    if (stripos($data['country'], $search) !== false) {
+                        echo '<div class="result-container">';
+                        echo "<h5>Country ID: {$data['id']}</h5>";
+                        echo "<h5>Country Name: {$data['country']}</h5>";
+                        echo "<h5>Country description: {$data['description']}</h5>";
+                        echo "<h5>Country latitude: {$data['latitude']}</h5>";
+                        echo "<h5>Country longitude: {$data['longitude']}</h5>";
+                        echo '</div>';
+                    }
+                }
+                echo '<div> <a href="index.php" class="btn">Back to Home</a> </div>';
+            }
+        }
+    }
+}
+
+// Function to call the API using cURL
+function callApi($url)
+{
+    $ch = curl_init($url);
+
+    if ($ch === false) {
+        return false;
+    }
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    // Execute cURL request
+    $response = curl_exec($ch);
+
+    // Check for cURL errors
+    if ($response === false) {
+        echo 'cURL Error: ' . curl_error($ch);
+        return false;
+    }
+
+    // Close cURL session
+    curl_close($ch);
+
+    return $response;
+}
+?>
+</body>
+</html>
